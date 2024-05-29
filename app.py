3aa0d5520dd54527ac974d87156ccd28f0193653
Rostyslav Hundarev <r.hundarev.im21@kpi.ua>
2024-05-29 21:44:25 +0300

    add solving sudoku function

--- app.py
+++ app.py
@@ -18,3 +18,23 @@ def is_valid(board, row, col, num):
                 return False
 
     return True
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i, j] == 0:
+                return (i, j)
+    return None
+
+def solve_sudoku(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    row, col = empty
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row, col] = num
+            if solve_sudoku(board):
+                return True
+            board[row, col] = 0
+    return False
